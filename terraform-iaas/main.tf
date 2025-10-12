a6cd5515feef611fe20f4306be84abd2ae63a6f1
Mohamed AZDOUD <mohamed.azd49@gmail.com>
2025-10-12 17:58:47 +0200

    fix(iaas): add an azure backend to synchronize state

--- terraform-iaas/main.tf
+++ terraform-iaas/main.tf
@@ -7,6 +7,13 @@ terraform {
   }
 
   required_version = ">= 1.1.0"
+
+  backend "azurerm" {
+    resource_group_name  = "rg-nan_1"
+    storage_account_name = "terracloudstate28602"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
