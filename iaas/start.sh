1c61ef40f7db48343fdc6a1275b783dcd9047200
WSL Renaissance <raphael.eouzan@gmail.com>
2025-11-20 15:31:15 +0100

    update start.sh

--- iaas/start.sh
+++ iaas/start.sh
@@ -6,3 +6,7 @@ echo "Initialisation de Terraform..."
 terraform init -reconfigure
 echo "Application de la configuration Terraform..."
 terraform apply -var-file="secret.tfvars" -auto-approve
+
+cd ../ansible
+echo "Ansible"
+ansible-playbook playbooks/init_terra_cloud.yml --limit terra_cloud_app
