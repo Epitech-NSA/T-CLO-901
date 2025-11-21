cf4adcf93ea989404f48384d3852fa66571bd44b
Mohamed AZDOUD <mohamed.azd49@gmail.com>
2025-11-21 16:33:11 +0100

    fix(iaas): acr password is now automatically provided to Ansible in start.sh

--- iaas/start.sh
+++ iaas/start.sh
@@ -8,6 +8,8 @@ terraform init -reconfigure
 echo "Application de la configuration Terraform..."
 terraform apply -var-file="secret.tfvars" -auto-approve
 
+ACR_PASSWORD=$(terraform output -raw acr_password)
+
 cd ../ansible
 echo "Ansible"
-ansible-playbook playbooks/init_terra_cloud.yml --limit terra_cloud_app
+ansible-playbook playbooks/init_terra_cloud.yml --limit terra_cloud_app --extra-vars "acr_password=$ACR_PASSWORD"
