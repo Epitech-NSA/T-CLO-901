9de9b31e456e38f759764a2ec2921c4924b8b07e
Mohamed AZDOUD <mohamed.azd49@gmail.com>
2025-11-22 20:43:30 +0100

    feat(iaas) : add Network Security Group and link it to the app subnet

--- iaas/terraform/network/main.tf
+++ iaas/terraform/network/main.tf
@@ -26,3 +26,54 @@ resource "azurerm_subnet" "subnet_main" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.subnet.address_prefix]
 }
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "tc-nsg-iaas-frc-01"
+  location            = var.rg_location
+  resource_group_name = var.rg_name
+
+  security_rule {
+
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+
+    name                       = "HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTPS"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+}
+
+
+
+resource "azurerm_subnet_network_security_group_association" "subnet_nsg_assoc" {
+  subnet_id                 = azurerm_subnet.subnet_main.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
